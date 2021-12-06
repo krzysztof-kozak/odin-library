7cd098d42f64c2e20aa58eb007179651f7d947f4
Kris <krzysztofkoz6@gmail.com>
2021-12-06 21:16:22 +0100

    Render books from the library

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 
-function Book(tite, author, pages, hasBeenRead) {
-	this.tite = tite;
+function Book(title, author, pages, hasBeenRead) {
+	this.tite = title;
 	this.author = author;
 	this.pages = pages;
 	this.hasBeenRead = hasBeenRead;
@@ -12,7 +12,6 @@ function addBookToLibrary(book) {
 }
 
 // Add some books manually to get started.
-
 const book1 = new Book("Eloquent JavaScript", "Marijn Haverbeke", 472, false);
 const book2 = new Book("Head First JavaScript Programming", "Elisabeth Robson", 704, false);
 const book3 = new Book(
@@ -27,3 +26,25 @@ addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 addBookToLibrary(book4);
+
+// Show books from our pre-populated library;
+const library = document.querySelector(".library");
+
+function renderBooks() {
+	for (const book in myLibrary) {
+		const bookDiv = document.createElement("div");
+
+		bookDiv.classList.add("card");
+		bookDiv.setAttribute("aria-roledescription", "book card");
+
+		bookDiv.innerHTML = `
+	<h2 class="card__title">${book.title}</h2>
+	<p>${book.author}</p>
+	<p>${book.pages}</p>
+	<p>Has Been Read: ${book.hasBeenRead ? "Yes" : "No"}</p>`;
+
+		library.appendChild(bookDiv);
+	}
+}
+
+renderBooks();
